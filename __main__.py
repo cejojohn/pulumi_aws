e9e5ff7d7cf9410e06107161cabbab2190ccaee5
Cejo John <cejokjohn@gmail.com>
2022-09-22 20:57:43 +1000

    Create bucket and upload index page

--- __main__.py
+++ __main__.py
@@ -1,11 +1,33 @@
 """An AWS Python Pulumi program"""
 import os
 
-from pulumi import export
+from pulumi import export, FileAsset
 from pulumi_aws import s3
+import mimetypes
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+
+
+# Create S3 Bucket with page
+bucket = s3.Bucket('my-bucket1', 
+    acl="public-read",
+    website=s3.BucketWebsiteArgs(index_document="index.html"),
+    tags={
+        "Environments": "Dev",
+        "Name": "mybucket",
+        },
+    )
+
+cwd = os.path.dirname(__file__)
+content_file = "index.html"
+file_fullpath = os.path.join(cwd, content_file)
+mime_type, _ = mimetypes.guess_type(file_fullpath)
+
+s3_obj = s3.BucketObject(content_file, 
+    bucket=bucket.id,
+    source=FileAsset(file_fullpath),
+    content_type=mime_type,
+    acl="public-read",
+    )
 
 # Export the name of the bucket
-export('bucket_name', bucket.id)
+export('bucket_name', bucket.website_endpoint)
